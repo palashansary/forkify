438c8c24be4da27dd00d2b278682f1ef48f43954
Abdul Hai Siddiki Ansary <82629461+palashansary@users.noreply.github.com>
2023-08-13 10:04:01 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '3d346d53-66cd-4b9c-b23a-fefc831d9ad1';
+export const KEY = 'YOUR_API_KEY';
 export const MODAL_CLOSE_SEC = 2.5;
